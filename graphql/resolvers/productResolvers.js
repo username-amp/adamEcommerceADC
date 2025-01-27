d2a260a2aea8e624e4e458deb9cacbb8295249dd
username-amp <ramosrichardjr25@gmail.com>
2025-01-27 23:29:02 +0800

    adding display product resolver in productResolvers

--- graphql/resolvers/productResolvers.js
+++ graphql/resolvers/productResolvers.js
@@ -1,19 +1,36 @@
 const { GraphQLString, GraphQLNonNull, GraphQLObjectType } = require("graphql");
 const Product = require("../../models/Product");
+const ProductType = require(`../types/ProductType`);
+const ResponseType = require(`../types/ResponseType`);
 
-// Product Type that represents the product model
-const ProductType = new GraphQLObjectType({
-  name: "Product",
-  fields: {
-    name: { type: GraphQLString },
-    description: { type: GraphQLString },
-    category: { type: GraphQLString },
-    price: { type: GraphQLString },
-    solds: { type: GraphQLString },
-    images: { type: GraphQLString },
-    stock: { type: GraphQLString },
-    rating: { type: GraphQLString },
-    tags: { type: GraphQLString },
+/* ----- Resolver for Display Product ----- */
+const displayProduct = {
+  type: ResponseType,
+  resolve: async () => {
+    try {
+      const products = await Product.find();
+
+      if (products.length === 0) {
+        return {
+          code: 204,
+          status: false,
+          message: "No Product found",
+        };
+      }
+
+      return {
+        code: 200,
+        status: true,
+        message: JSON.stringify(products),
+      };
+    } catch (error) {
+      return {
+        code: 500,
+        status: false,
+        message: ("Internal server error", error.message),
+      };
+    }
   },
-});
+};
 
+module.exports = { displayProduct };
